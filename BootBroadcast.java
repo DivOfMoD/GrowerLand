3fd2431120229a44307e97dc03143bd67fccfd71
Dmitry <dreik.amber@yandex.ru>
2017-04-28 20:09:42 +0300

    BS, чтобы сервис сартовал после перезагрузки телефона

---
+++ BootBroadcast.java
@@ -0,0 +1,13 @@
+package com.divofmod.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+ //BroadcastReceiver, чтобы сервис запускался  после перезагрузки устройства.
+public class BootBroadcast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, CheckWeather.class));
+    }
+}

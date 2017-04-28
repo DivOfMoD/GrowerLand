191b49c173b4d6cac84de79fce5ddb9f583a4e41
maxcriser <mv.maxcriser@gmail.com>
2017-04-28 20:05:17 +0300

    ref

---
+++ GrowerLand/app/src/main/java/com/example/nick/growerland/ui/AddGardenActivity.java
@@ -0,0 +1,15 @@
+package com.example.nick.growerland.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nick.growerland.R;
+
+public class AddGardenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_garden);
+    }
+}
